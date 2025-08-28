3e87f007053b5773384e094c94816d7b408fb18a
uwu-gl <moegluwu0@gmail.com>
2025-08-28 09:34:50 +0800

    bt-kernel: Import oplus changes
    Change-Id: I67bfe974d9cd09f6ab01d6b0abf89adf421a29be
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- slimbus/btfm_slim.c
+++ slimbus/btfm_slim.c
@@ -92,6 +92,10 @@ int btfm_slim_read(struct btfmslim *btfmslim, uint32_t reg, uint8_t pgd)
 	return ret;
 }
 
+//#ifdef OPLUS_ARCH_EXTENDS
+extern int oplus_bt_timeout_status;
+extern int oplus_bt_init_err;
+//#endif /* OPLUS_ARCH_EXTENDS */
 int btfm_slim_enable_ch(struct btfmslim *btfmslim, struct btfmslim_ch *ch,
 	uint8_t rxport, uint32_t rates, uint8_t nchan)
 {
@@ -158,6 +162,9 @@ int btfm_slim_enable_ch(struct btfmslim *btfmslim, struct btfmslim_ch *ch,
 		goto error;
 	}
 
+//#ifdef OPLUS_ARCH_EXTENDS
+	oplus_bt_timeout_status = 0;
+//#endif /* OPLUS_ARCH_EXTENDS */
 	if (ret == 0)
 		btfm_num_ports_open++;
 	BTFMSLIM_INFO("btfm_num_ports_open: %d", btfm_num_ports_open);
@@ -167,6 +174,12 @@ error:
 			ret, btfm_num_ports_open);
 	kfree(chan->dai.sconfig.chs);
 	chan->dai.sconfig.chs = NULL;
+//#ifdef OPLUS_ARCH_EXTENDS
+	if (ret == -ETIMEDOUT && oplus_bt_timeout_status < 1000) {
+		oplus_bt_timeout_status++;
+		BTFMSLIM_ERR("ret = %d, oplus_bt_timeout_status %d", ret, oplus_bt_timeout_status);
+	}
+//#endif /* OPLUS_ARCH_EXTENDS */
 	return ret;
 }
 
@@ -249,7 +262,10 @@ int btfm_slim_disable_ch(struct btfmslim *btfmslim, struct btfmslim_ch *ch,
 		chipset_ver == QCA_HAMILTON_SOC_ID_0200 ||
 		chipset_ver == QCA_APACHE_SOC_ID_0100 ||
 		chipset_ver == QCA_APACHE_SOC_ID_0110 ||
-		chipset_ver == QCA_APACHE_SOC_ID_0121)) {
+		chipset_ver == QCA_APACHE_SOC_ID_0121 ||
+		chipset_ver == QCA_MOSELLE_SOC_ID_0100 ||
+		chipset_ver == QCA_MOSELLE_SOC_ID_0110 ||
+		chipset_ver == QCA_MOSELLE_SOC_ID_0120)) {
 		BTFMSLIM_INFO("SB reset needed after all ports disabled, sleeping");
 		msleep(DELAY_FOR_PORT_OPEN_MS);
 	}
@@ -501,7 +517,16 @@ int btfm_slim_hw_init(struct btfmslim *btfmslim)
 	 * enabled status
 	 */
 	btfmslim->enabled = 1;
+//#ifdef OPLUS_ARCH_EXTENDS
+	oplus_bt_init_err = 0;
+//#endif /* OPLUS_ARCH_EXTENDS */
 error:
+//#ifdef OPLUS_ARCH_EXTENDS
+	if (ret == -ETIMEDOUT && oplus_bt_init_err < 1000) {
+		oplus_bt_init_err++;
+		BTFMSLIM_ERR("ret = %d, oplus_bt_init_err %d", ret, oplus_bt_init_err);
+	}
+//#endif /* OPLUS_ARCH_EXTENDS */
 	mutex_unlock(&btfmslim->io_lock);
 	return ret;
 }
